58839ed3812ef38f50e6d492658ebabe43de5c4b
pyropulse137 <64796650+jplasch@users.noreply.github.com>
2025-04-05 16:44:01 -0700

    Begin creation of hat-finder game

---
+++ main.js
@@ -0,0 +1,55 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+  constructor(field) {
+    this.field = field;
+    this.playerInput = null;
+    // Player position
+    // [y-coord, x-coord]
+    this.playerPosition = [0, 0];
+  }
+
+  print() {
+    const regex = /,/g;
+    const field = this.field.join('\n').replace(regex, '');
+    console.log(field);
+  }
+
+  promptPlayer() {
+    this.playerInput = prompt('Which way? ');
+  }
+
+  assessInput() {
+
+  }
+
+  checkHole() {
+    const [x, y] = this.playerPosition;
+    if (this.field[x][y] === hole) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const myField = new Field([
+  ['*', '░', '░', '░', 'O', '░', '░', '░', '░', '░', '░'],
+  ['O', '░', 'O', 'O', '░', '░', 'O', '░', 'O', '░', '░'],
+  ['░', '░', '░', 'O', '░', 'O', '░', '░', '░', '░', 'O'],
+  ['░', 'O', 'O', '░', '░', 'O', '░', 'O', '░', 'O', '░'],
+  ['░', '░', '░', '░', 'O', '░', '░', '░', 'O', '░', '░'],
+  ['░', 'O', '░', 'O', '░', '░', 'O', 'O', '░', '░', '░'],
+  ['░', 'O', '░', 'O', '░', '░', '░', 'O', '^', 'O', '░'],
+  ['░', 'O', '░', 'O', '░', 'O', '░', '░', 'O', 'O', '░'],
+  ['░', '░', '░', 'O', '░', '░', 'O', '░', 'O', '░', '░'],
+  ['░', 'O', '░', '░', 'O', '░', '░', 'O', '░', '░', '░'],
+  ['O', '░', '░', 'O', '░', '░', '░', '░', '░', '░', '░',],
+]);
+
+myField.print();
